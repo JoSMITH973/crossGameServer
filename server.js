53f9f9c8c1759408010e471fbee5658944e9c8b2
JoSMITH973 <72966617+JoSMITH973@users.noreply.github.com>
2021-06-07 15:47:38 +0200

    début socket.io

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send(
-      "<h1>Hello World!</h1><br/><h3>Bienvenue sur notre platforme</h3>"
-      )
-})
-app.get('/yo', (req,res) => {
-    res.send('test ok')
-    console.log("test ok en console")
-})
+app.get("/", (req, res) => {
+    // res.send("<h1>Hello World!</h1><br/><h3>Bienvenue sur notre platforme</h3>");
+    res.sendFile(__dirname + "/index.html");
+});
 
-app.post('/postt', (req,res) =>{
-    res.send('test post')
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("player's nickname", (msg) => {
+        console.log("player's nickname: " + msg);
+    });
+    socket.broadcast.emit('hi');
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
 });
 
 server.listen(port, () => {
-  console.log(`server listening on port:${port}`)
-})
+    console.log(`server listening on port:${port}`);
+});
