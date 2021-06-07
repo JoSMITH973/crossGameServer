4e49af46d170a5626bd10f2957473ea660cfaf6f
JoSMITH973 <72966617+JoSMITH973@users.noreply.github.com>
2021-06-07 15:52:55 +0200

    début socket.io

--- server.js
+++ server.js
@@ -7,19 +7,35 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get("/", (req, res) => {
-    // res.send("<h1>Hello World!</h1><br/><h3>Bienvenue sur notre platforme</h3>");
-    res.sendFile(__dirname + "/index.html");
+app.get("/", (req,res) => {
+    io.on("connection", (socket) => {
+        res.send("Welcome");
+    })
+})
+
+app.post("/create", (req, res) => {
+    io.on("connection", (socket) => {
+        console.log("Creating the session");
+        socket.on("player's nickname", (msg) => {
+            console.log("player's nickname : " + msg);
+        });
+        socket.broadcast.emit('hi');
+        socket.on("disconnect", () => {
+            console.log("user disconnected");
+        });
+    });
 });
 
-io.on("connection", (socket) => {
-    console.log("a user connected");
-    socket.on("player's nickname", (msg) => {
-        console.log("player's nickname: " + msg);
-    });
-    socket.broadcast.emit('hi');
-    socket.on("disconnect", () => {
-        console.log("user disconnected");
+app.post("/join", (req, res) => {
+    io.on("connection", (socket) => {
+        console.log("Joinning the session of ");
+        socket.on("player's nickname", (msg) => {
+            console.log("player's nickname: " + msg);
+        });
+        socket.broadcast.emit('hi');
+        socket.on("disconnect", () => {
+            console.log("user disconnected");
+        });
     });
 });
 
