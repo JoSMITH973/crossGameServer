809290e95f02485513e882288e4853c432422a8f
JoSMITH973 <72966617+JoSMITH973@users.noreply.github.com>
2021-06-08 11:04:41 +0200

    échange message socket.io

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const port = process.env.PORT;
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    // res.sendFile(__dirname + "/index.html");
+    res.send("Welcome");
     // io.on("connection", (socket) => {
     //     res.send("Welcome");
     // })
