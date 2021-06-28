451fa767f076b690f4828535722882372a3f11b9
JoSMITH973 <72966617+JoSMITH973@users.noreply.github.com>
2021-06-28 10:04:25 +0200

    correction serveur

--- server.js
+++ server.js
@@ -18,21 +18,23 @@ app.get("/MagicNumber2", (req, res) => {
 
 let randomNumber = Math.floor(Math.random() * 1348);
 io.on("connection", (socket) => {
-    console.log('userConnected')
+    console.log('user connected')
     
     socket.on('createRoom', (userName) => {
         console.log(userName);
         let roomId = uniqid.time();
         socket.join(roomId);
-        io.to(roomId).emit('createRoom',roomId)
+        io.in(roomId).emit('createRoom',roomId)
     })
 
     socket.on("joinRoom", (room) => {
         console.log('joinRoom :',room);
+        console.log("in Join :",io.sockets.adapter.rooms.get(room));
         socket.join(room);
     });
-
+    
     socket.on("magicNumber", (room, userName, numberPicked) => {
+        console.log('session :', io.of("/").adapter.rooms.get(room).size );
         socket.join(room);
         console.log('room :',room,'username :',userName,'numberPicked :',numberPicked);
         let operator = "=";
@@ -41,20 +43,21 @@ io.on("connection", (socket) => {
 
         if(numberPicked === randomNumber) {
             // io.emit('createRoom',socket.id, userName+" says the number is "+numberPicked);
-            io.to(room).emit('magicNumber',room, userName, numberPicked, operator);
+            io.in(room).emit('magicNumber',room, userName, numberPicked, operator);
             randomNumber = Math.floor(Math.random() * 1348)
         }
         else{
             // Si le chiffre choisi est PLUS PETIT que le chiffre aléatoire alors signe ( < ) sinon ( > )
             operator = (numberPicked < randomNumber) ? "<" : ">";
-            io.to(room).emit('magicNumber',room, userName, numberPicked, operator);
+            io.in(room).emit('magicNumber',room, userName, numberPicked, operator);
         }
 
         console.log(randomNumber);
     })
 
-    // socket.join()
-    
+    socket.on("disconnect", () => {
+        console.log('user disconnected');
+    })
 });
 
 http.listen(port, () => {
