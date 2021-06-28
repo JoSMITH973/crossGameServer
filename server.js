2b5781a70876e1c674874a63a7e6ce5ff85e6d0d
JoSMITH973 <72966617+JoSMITH973@users.noreply.github.com>
2021-06-28 15:35:54 +0200

    correction serveur

--- server.js
+++ server.js
@@ -17,25 +17,61 @@ app.get("/MagicNumber2", (req, res) => {
 });
 
 let randomNumber = Math.floor(Math.random() * 1348);
+let usersArray = [];
 io.on("connection", (socket) => {
     console.log('user connected')
     
     socket.on('createRoom', (userName) => {
-        console.log(userName);
         let roomId = uniqid.time();
         socket.join(roomId);
-        io.in(roomId).emit('createRoom',roomId)
+        io.in(roomId).emit('createRoom',roomId);
+        console.log(userName,' create a session : ',roomId);
+        usersArray.push(
+            {
+                roomId: roomId,
+                players: {
+                    host: {
+                        socketId: socket.id,
+                        name: userName
+                    },
+                    guest: null
+                }
+            }
+        );
+        console.log('array :',usersArray);
     })
 
-    socket.on("joinRoom", (room) => {
+    socket.on("joinRoom", (room, userName) => {
         console.log('joinRoom :',room);
-        console.log("in Join :",io.sockets.adapter.rooms.get(room));
-        socket.join(room);
+        let clientsNumberInSession = io.of("/").adapter.rooms.get(room).size;
+        console.log('session number before join :', clientsNumberInSession );
+        
+        let indexRoom = usersArray.findIndex( element => element.roomId == room);
+        console.log('index :',indexRoom);
+        if(clientsNumberInSession === 1) {
+            let players = usersArray[indexRoom].players;
+            players.guest = {
+                socketId: socket.id,
+                name: userName
+            }
+            console.log('this array :', usersArray[indexRoom]);
+            socket.join(room);
+            console.log('session number after join :', clientsNumberInSession );
+            return io.in(room).emit("joinRoom","=", players.host.name, players.guest.name);
+        }
+        else {
+            console.log('impossible to join')
+            return socket.emit("joinRoom","!")
+        }
     });
     
     socket.on("magicNumber", (room, userName, numberPicked) => {
-        console.log('session :', io.of("/").adapter.rooms.get(room).size );
-        socket.join(room);
+        // let clientsNumberInSession = io.of("/").adapter.rooms.get(room).size;
+        // console.log('session :', clientsNumberInSession );
+        // if(clientsNumberInSession > 2) {
+        //     return socket.emit('magicNumber',room, userName, 0, "!");
+        // }
+        // socket.join(room);
         console.log('room :',room,'username :',userName,'numberPicked :',numberPicked);
         let operator = "=";
         console.log('id room reçu :',room)
@@ -49,7 +85,8 @@ io.on("connection", (socket) => {
         else{
             // Si le chiffre choisi est PLUS PETIT que le chiffre aléatoire alors signe ( < ) sinon ( > )
             operator = (numberPicked < randomNumber) ? "<" : ">";
-            io.in(room).emit('magicNumber',room, userName, numberPicked, operator);
+            // io.in(room).emit('magicNumber',room, userName, numberPicked, operator);
+            io.to(room).emit('magicNumber',room, userName, numberPicked, operator);
         }
 
         console.log(randomNumber);
